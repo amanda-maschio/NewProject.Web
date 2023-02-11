6c74ccdc7f938857eb448c7ef87d4711a8d0e2e4
Amanda Maschio <amanda.maschio.skeel@gmail.com>
2023-02-10 22:51:41 -0300

    Consumer Home Page HTML

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { ConfirmModalComponent} from './confirm-modal/confirm-modal.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatChipsModule } from '@angular/material/chips';
+import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask'
+import { MaterialModule } from './material/material.module';
 
 
 @NgModule({
@@ -19,19 +15,14 @@ import { NgxMaskModule } from 'ngx-mask'
   ],
   imports: [
     CommonModule,
-    MatIconModule,
     BsDropdownModule.forRoot(),
     CollapseModule.forRoot(),
     ModalModule.forRoot(),
-    MatCardModule,
-    MatProgressBarModule,
-    MatProgressBarModule,
-    MatFormFieldModule,
-    MatChipsModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxMaskModule.forRoot()
-    ],
+    NgxMaskModule.forRoot(),
+    MaterialModule
+  ],
   exports: [
     ConfirmModalComponent
   ],
